d2f8681d77847b460a095d7d42a5909d5d56fcdd
Ram Chandra Siuli <ram_Chandra.siuli@syngenta.com>
2021-04-10 02:19:11 +0530

    - lint config updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
+  root: true,
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
   rules: {
+    'import/prefer-default-export': 0,
+    'class-methods-use-this': 0,
+    'no-console': 0,
+    'no-plusplus': 'off',
+    'no-param-reassign': 0,
+    'global-require': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'no-unused-expressions': 0,
+    'import/extensions': 0,
+    'no-use-before-define': ['error', { variables: false }],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'import/no-cycle': [0, { maxDepth: 1 }],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.js', '.jsx', 'ts', 'tsx'],
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        singleQuote: true,
+        printWidth: 100,
+        trailingComma: 'es5',
+        tabWidth: 2,
+        semi: true,
+      },
+    ],
   },
+  env: {
+    jest: true,
+  },
+  plugins: ['prettier'],
 };
