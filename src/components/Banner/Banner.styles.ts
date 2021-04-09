42dfe04bf63a93d2fd9630b832e9142881fa3ec1
Ram Chandra Siuli <ram_Chandra.siuli@syngenta.com>
2021-04-10 02:12:30 +0530

    - eslint added

--- src/components/Banner/Banner.styles.ts
+++ src/components/Banner/Banner.styles.ts
@@ -1,7 +1,10 @@
 import { StyleSheet } from 'react-native';
 
 const BannerStyle = StyleSheet.create({
-  bannerText: { fontWeight: '400', fontSize: 18 },
+  bannerText: {
+    fontWeight: '400',
+    fontSize: 18,
+  },
 });
 
 export default BannerStyle;
